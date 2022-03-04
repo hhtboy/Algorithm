23261d126cd20dc849cb561e812b7c5cce3abefe
hhtboy <hhtboy1024@gmail.com>
2022-03-05 00:50:31 +0900

    third commit

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 int main()
 {
 
-    me hhtboy;
-    you ohsgirl;
-    hhtboy.Init(1234567);
-    hhtboy.ShowInfo();
+    //삭제 부분
     std::cout << "hello";
     std::cout << "수정된 부분입니다.";
     std::cout << "맥에서 수정했습니다.";
     // hhtboy.SendText(ohsgirl, hhtboy.myPhoneNum);
+}
+
+void add(int x, int y)
+{
+    x + y;
 }
