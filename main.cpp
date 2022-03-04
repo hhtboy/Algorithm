4dfe851f46872aa4b2cde5ddf1d41cb620839cc0
hhtboy <hhtboy1024@gmail.com>
2022-03-04 14:05:13 +0900

    mac first commit

--- main.cpp
+++ main.cpp
@@ -10,5 +10,6 @@ int main()
     hhtboy.ShowInfo();
     std::cout << "hello";
     std::cout << "수정된 부분입니다.";
+    std::cout << "맥에서 수정했습니다.";
     // hhtboy.SendText(ohsgirl, hhtboy.myPhoneNum);
 }
