52ffe34c2c76bc98cc82c9e117d028e2328fe420
hhtboy <hhtboy1024@gmail.com>
2022-04-19 19:35:14 +0900

    과제 제출

--- 과제1_2019124206_황현택/Homework1/Homework1/Homework1/Rectangle.h
+++ 과제1_2019124206_황현택/Homework1/Homework1/Homework1/Rectangle.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 
@@ -11,26 +11,24 @@ private:
 	int xLow, yLow;
 
 public:
-	//�?�?�?, �????
+	//생성자, 파괴자
 	Rectangle(int, int, int, int);
 	Rectangle();
 	~Rectangle();
 
-	//�?�? ??? �?�?
+	//넓이 계산 함수
 	void CulRectArea();
 
-	//???�? �?�?�? ����? �??� ???
+	//겹치는 영역의 객체를 생성
 	Rectangle OverlapRect(Rectangle& r1, Rectangle& r2);
 
-	//�?,??? �?�?�? �????�?
+	//입출력 연산자 오버로딩
 	friend std::ostream& operator <<(std::ostream&, Rectangle&); 
 	friend std::istream& operator >>(std::istream&, Rectangle&); 
 
 	Rectangle operator +(Rectangle&);
 
-	//�?�??� �?�? ??? Max,Min �?�?
+	//편의를 위해 만든 함수
 	int Max(int a, int b);
-
-
 	int Min(int a, int b);
 };
